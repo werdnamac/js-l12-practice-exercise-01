526a2f739a9241875ec17332c14894602287751d
andy <werdnamac@gmail.com>
2022-04-26 22:18:15 -0400

    added random imagegenerator for five users, returning picture, first name, and country

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function() {
+    const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+    let data = await usersRequest.json();
+    const userResults = data.results;
+    displayUsers(userResults)
+};
+
+const displayUsers = function(userResults) {
+    randomFolks.innerHTML = "";
+    for (let user of userResults) {
+        let country = user.nat;
+        let name = user.name.first;
+        let imageUrl = user.picture.medium;
+        let userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+        `;
+        randomFolks.append(userDiv);
+    }
+}
+
+getData();
