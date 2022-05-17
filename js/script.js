841456ddace9a6b52649cb35a1eae4d8517ed2d4
andy <werdnamac@gmail.com>
2022-05-16 20:43:33 -0400

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function() {
-    const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+const getData = async function(numUsers) {
+    const usersRequest = await fetch (`https://randomuser.me/api?results=${numUsers}`);
     let data = await usersRequest.json();
     const userResults = data.results;
     displayUsers(userResults)
@@ -23,4 +24,10 @@ const displayUsers = function(userResults) {
     }
 }
 
-getData();
+
+selectUserNumber.addEventListener("change", function (e) {
+    let numUsers = e.target.value;
+    getData(numUsers);
+});
+
+getData(1);
